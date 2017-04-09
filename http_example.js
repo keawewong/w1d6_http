ae5757f895a10ec658b95e17c3b028535fdd6b96
Keawe <account@keawewong.com>
2017-04-09 14:27:09 -0400

    Modified the callback function to receive the data

--- http_example.js
+++ http_example.js
@@ -8,7 +8,10 @@ var options = {
 // called by https when the request is made.
 var callback = function(response) {
   console.log('In response handler callback!');
-  console.log('Response: ', response);
+  response.on('data', function(chunk) {
+    console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
+    console.log(chunk.toString());
+  });
 }
 
 console.log("I'm about to make the request!");
@@ -16,4 +19,3 @@ console.log("I'm about to make the request!");
 https.request(options, callback).end();
 
 console.log("I've made the request!");
-
